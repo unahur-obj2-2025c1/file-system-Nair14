cc88ae614f6f2460f44db7faa082f243d72d2a48
Nair14 <nairpaz014@gmail.com>
2025-05-03 18:45:19 -0300

    practica

--- src/main/java/ar/edu/unahur/obj2/FileSystem.java
+++ src/main/java/ar/edu/unahur/obj2/FileSystem.java
@@ -1,5 +1,17 @@
 package ar.edu.unahur.obj2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileSystem {
+    private List<IElemento> elementos = new ArrayList<>();
+
+    public void agregarElemento(IElemento elemento) {
+        elementos.add(elemento);
+    }
+
+    public void listar() {
+        elementos.stream().forEach(elementos -> {elementos.mostrar(0);});
+    }
 
 }
